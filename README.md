ccfb3e53f8fbdbb78225e5f9d7e3439b36235a5b
TheEntropyShard <theentropyshard@gmail.com>
2024-09-21 15:35:28 +0300

    Created the project

---
+++ README.md
@@ -0,0 +1,2 @@
+# ModMaker
+This is a simple mod maker for [Cosmic Reach](https://finalforeach.itch.io/cosmic-reach)
---
+++ src/main/java/me/theentropyshard/modmaker/Main.java
@@ -0,0 +1,7 @@
+package me.theentropyshard.modmaker;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
