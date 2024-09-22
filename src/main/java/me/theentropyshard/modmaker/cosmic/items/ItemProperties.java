a8e674bb4c2de56fca0b63da92706569e1453c10
TheEntropyShard <theentropyshard@gmail.com>
2024-09-22 18:16:25 +0300

    Added Item classes

---
+++ src/main/java/me/theentropyshard/modmaker/cosmic/items/ItemProperties.java
@@ -0,0 +1,71 @@
+/*
+ * ModMaker - https://github.com/TheEntropyShard/ModMaker
+ * Copyright (C) 2024 TheEntropyShard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.theentropyshard.modmaker.cosmic.items;
+
+public class ItemProperties {
+    private String texture;
+    private String modelType;
+    private int stackLimit;
+    private int toolSpeed;
+    private int durability;
+
+    public ItemProperties() {
+
+    }
+
+    public String getTexture() {
+        return this.texture;
+    }
+
+    public void setTexture(String texture) {
+        this.texture = texture;
+    }
+
+    public String getModelType() {
+        return this.modelType;
+    }
+
+    public void setModelType(String modelType) {
+        this.modelType = modelType;
+    }
+
+    public int getStackLimit() {
+        return this.stackLimit;
+    }
+
+    public void setStackLimit(int stackLimit) {
+        this.stackLimit = stackLimit;
+    }
+
+    public int getToolSpeed() {
+        return this.toolSpeed;
+    }
+
+    public void setToolSpeed(int toolSpeed) {
+        this.toolSpeed = toolSpeed;
+    }
+
+    public int getDurability() {
+        return this.durability;
+    }
+
+    public void setDurability(int durability) {
+        this.durability = durability;
+    }
+}
