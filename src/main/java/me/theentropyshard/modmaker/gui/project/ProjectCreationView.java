fcd52378b5813b22608cd7d1bf5dcd2bb0295f9d
TheEntropyShard <theentropyshard@gmail.com>
2024-10-01 21:49:32 +0300

    Spread things to separate classes

--- src/main/java/me/theentropyshard/modmaker/gui/project/ProjectCreationView.java
+++ src/main/java/me/theentropyshard/modmaker/gui/project/ProjectCreationView.java
@@ -20,11 +20,9 @@ package me.theentropyshard.modmaker.gui.project;
 
 import me.theentropyshard.modmaker.ModMaker;
 import me.theentropyshard.modmaker.utils.SwingUtils;
-import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 import java.util.Optional;
 
 public class ProjectCreationView extends JPanel {
@@ -57,113 +55,6 @@ public class ProjectCreationView extends JPanel {
             this.projectInfoForm.getNamespaceField().getText(),
             this.projectInfoForm.getVersionField().getText()
         );
-    }
-
-    private static final class ProjectInfoForm extends JPanel {
-        private final JTextField nameField;
-        private final JTextField namespaceField;
-        private final JTextField versionField;
-
-        public ProjectInfoForm() {
-            super(new MigLayout("fillx", "[][]", "[top][top][top][top]"));
-
-            JLabel nameLabel = new JLabel("Name:");
-            this.add(nameLabel);
-
-            this.nameField = new JTextField("untitled");
-            this.nameField.selectAll();
-            this.add(this.nameField, "wrap, growx, pushx");
-
-            JLabel namespaceLabel = new JLabel("Namespace:");
-            this.add(namespaceLabel);
-
-            this.namespaceField = new JTextField("untitled");
-            this.add(this.namespaceField, "wrap, growx, pushx");
-
-            LocationField locationField = new LocationField();
-            locationField.getPathField().setText(ModMaker.getInstance().getProjectsDir().toString());
-            locationField.getLocationLabel().setText("Project will be created in: " +
-                ModMaker.getInstance().getProjectsDir() + File.separator + this.nameField.getText());
-
-            SwingUtils.addChangeListener(this.nameField, e -> {
-                this.namespaceField.setText(
-                    this.nameField.getText()
-                        .toLowerCase()
-                        .replace(" ", "")
-                );
-
-                locationField.getLocationLabel().setText("Project will be created in: " +
-                    ModMaker.getInstance().getProjectsDir() + File.separator + this.nameField.getText());
-            });
-
-            JLabel versionLabel = new JLabel("Version:");
-            this.add(versionLabel);
-
-            this.versionField = new JTextField("0.0.1");
-            this.add(this.versionField, "wrap, growx, pushx");
-
-            this.add(new JLabel("Location:"));
-            this.add(locationField, "wrap, growx, pushx");
-        }
-
-        public JTextField getNameField() {
-            return this.nameField;
-        }
-
-        public JTextField getNamespaceField() {
-            return this.namespaceField;
-        }
-
-        public JTextField getVersionField() {
-            return this.versionField;
-        }
-    }
-
-    private static final class LocationField extends JPanel {
-        private final JTextField pathField;
-        private final JLabel locationLabel;
-
-        public LocationField() {
-            super(new MigLayout("fill, insets 0", "[]", "[][]"));
-
-            this.pathField = new JTextField();
-            this.add(this.pathField, "wrap, growx, pushx");
-
-            this.locationLabel = new JLabel();
-            this.add(this.locationLabel);
-        }
-
-        public JTextField getPathField() {
-            return this.pathField;
-        }
-
-        public JLabel getLocationLabel() {
-            return this.locationLabel;
-        }
-    }
-
-    public static final class ProjectCreationInfo {
-        private final String name;
-        private final String namespace;
-        private final String version;
-
-        public ProjectCreationInfo(String name, String namespace, String version) {
-            this.name = name;
-            this.namespace = namespace;
-            this.version = version;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-
-        public String getNamespace() {
-            return this.namespace;
-        }
-
-        public String getVersion() {
-            return this.version;
-        }
     }
 
     public static ProjectCreationView showDialog() {
