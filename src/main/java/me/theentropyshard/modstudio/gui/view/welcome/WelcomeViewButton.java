0759854cbc1540a5929f62d134f88dcc61c08fba
TheEntropyShard <theentropyshard@gmail.com>
2025-02-20 22:33:09 +0300

    Fixed WelcomeViewButton

--- src/main/java/me/theentropyshard/modstudio/gui/view/welcome/WelcomeViewButton.java
+++ src/main/java/me/theentropyshard/modstudio/gui/view/welcome/WelcomeViewButton.java
@@ -3,6 +3,7 @@ package me.theentropyshard.modstudio.gui.view.welcome;
 import me.theentropyshard.modstudio.gui.Icons;
 import me.theentropyshard.modstudio.gui.components.Card;
 import me.theentropyshard.modstudio.gui.utils.MouseClickListener;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -18,7 +19,7 @@ public class WelcomeViewButton extends JPanel {
     private final JLabel label;
 
     public WelcomeViewButton(String icon, String text) {
-        super(new BorderLayout());
+        super(new MigLayout("wrap, insets 0, gap 5 5", "[center]", "[center][top]"));
 
         this.button = new Card(0, 10) {
             @Override
@@ -38,21 +39,13 @@ public class WelcomeViewButton extends JPanel {
         };
         this.button.setLayout(new BorderLayout());
         this.button.add(new JLabel(Icons.get(icon).derive(32, 32)), BorderLayout.CENTER);
-        this.add(this.button, BorderLayout.CENTER);
-
-        this.add(Box.createHorizontalStrut(3), BorderLayout.WEST);
-        this.add(Box.createHorizontalStrut(3), BorderLayout.EAST);
+        this.add(this.button);
 
         this.label = new JLabel(text);
         this.label.setHorizontalAlignment(JLabel.CENTER);
         this.label.setBorder(new EmptyBorder(10, 0, 0, 0));
         this.label.setFont(this.label.getFont().deriveFont(13.0f));
-        this.add(this.label, BorderLayout.SOUTH);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        this.add(this.label);
     }
 
     public void addOnClickListener(Runnable r) {
